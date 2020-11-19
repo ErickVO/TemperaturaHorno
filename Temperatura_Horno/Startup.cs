2ef329cbb325e2ff9da5fa3087a89f76881f64f7
Frandy Francisco Mota Rodriguez <frandy_mota@ucne.edu.do>
2020-11-19 00:51:44 -0500

    Toast instalado

--- Temperatura_Horno/Startup.cs
+++ Temperatura_Horno/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Temperatura_Horno.Data;
+using Blazored.Toast;
 
 namespace Temperatura_Horno
 {
@@ -29,6 +30,7 @@ namespace Temperatura_Horno
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
